6370085f592fe8622ccea97114e70dc0783becca
David Florness <edwargix@gmail.com>
2017-07-28 18:04:17 -0500

    2-5

---
+++ chapters/2/5.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int any(char s1[], char s2[])
+{
+  int i, j;
+
+  for (i = 0; s1[i] != '\0'; ++i)
+    for (j = 0; s2[j] != '\0'; ++j)
+      if (s1[i] == s2[j])
+        return i;
+
+  return -1;
+}
+
+int main()
+{
+  char str[] = "hello world";
+  char first[] = "w ";
+  printf("%d\n", any(str, first));
+  return 0;
+}
+
